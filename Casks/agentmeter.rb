8c77db9a5487f0a1e7ceaeba93888afbed2b9091
Felix Torres <felix@localhost>
2026-07-23 11:58:09 -0500

    agentmeter 1.6.6

--- Casks/agentmeter.rb
+++ Casks/agentmeter.rb
@@ -1,6 +1,6 @@
 cask "agentmeter" do
-  version "1.6.5"
-  sha256 "f4ae33688df882220acb31cc0fab4abc8cd0d0deb92ce5b100e3c523da6ecd9f"
+  version "1.6.6"
+  sha256 "93dc0f8dd9a8c93c456ff440f24f062bf7f532ba214e6db5352f87a6914e68f2"
 
   url "https://github.com/fdtorres1/AgentMeter/releases/download/v#{version}/AgentMeter.zip"
   name "AgentMeter"
