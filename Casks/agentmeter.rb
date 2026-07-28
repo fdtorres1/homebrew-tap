001101130a4ea13b8f4fec17209d1cb574e7fb41
fdtorres1 <163592391+fdtorres1@users.noreply.github.com>
2026-07-28 16:56:55 -0500

    agentmeter 1.9.0 (adds agentmeter CLI binary)

--- Casks/agentmeter.rb
+++ Casks/agentmeter.rb
@@ -1,6 +1,6 @@
 cask "agentmeter" do
-  version "1.8.0"
-  sha256 "5a4761af22f860d87f2297d5fdd5341b39036e1e499cb736db21b24a97c49d92"
+  version "1.9.0"
+  sha256 "630936630c84acbe05f8c59e2af8bcb490e01fe8eda3666f3c85cbe1a8c917f8"
 
   url "https://github.com/fdtorres1/AgentMeter/releases/download/v#{version}/AgentMeter.zip"
   name "AgentMeter"
@@ -16,6 +16,7 @@ cask "agentmeter" do
   depends_on macos: :sonoma
 
   app "AgentMeter.app"
+  binary "#{appdir}/AgentMeter.app/Contents/Helpers/agentmeter"
 
   zap trash: [
     "~/Library/Preferences/com.felixtorres.agentmeter.plist",
