bfedcb53b6cf0bf926b63e07ff65e439a749f7c7
fdtorres1 <163592391+fdtorres1@users.noreply.github.com>
2026-09-21 02:39:21 -0500

    agentmeter 1.11.3

--- Casks/agentmeter.rb
+++ Casks/agentmeter.rb
@@ -1,6 +1,6 @@
 cask "agentmeter" do
-  version "1.11.2"
-  sha256 "32cae930e5fb302564e3f2758ccfbebb1701df034018b367a89ffdb5cbc366cc"
+  version "1.11.3"
+  sha256 "c26ff38a77be84b7c496dbd4b790cba0b62f3454b6ac3a7f468c4e321e95c9eb"
 
   url "https://github.com/fdtorres1/AgentMeter/releases/download/v#{version}/AgentMeter.zip"
   name "AgentMeter"
